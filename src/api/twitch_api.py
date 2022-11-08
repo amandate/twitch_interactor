a43f3b19fb5e06fd63a8e19a91b8b4494bf7d0c7
Amanda Te <amandate@Amandas-MacBook-Pro.local>
2022-11-07 23:33:03 -0700

    added comments for api package

--- src/api/twitch_api.py
+++ src/api/twitch_api.py
@@ -1,10 +1,17 @@
 import requests
 
+"""
+    Represents interaction with the Twitch API.
+"""
+
 class TwitchAPI:
     def __init__(self, client_id, access_token=None):
         self.client_id = client_id
         self.access_token = access_token
 
+    '''
+        Returns the headers to be used when making calls to the API.
+    '''
     def _get_request_headers(self):
         headers = {
             "Client-ID": self.client_id,
@@ -15,6 +22,10 @@ class TwitchAPI:
 
         return headers
 
+    '''
+        Sends a GET request to the given endpoint and params and returns its
+        response.
+    '''
     def _request_get(self, endpoint, params=None, json=True):
         headers = self._get_request_headers()
         get_response = requests.get(endpoint, params=params, headers=headers)
--- src/api/users_api.py
+++ src/api/users_api.py
@@ -2,12 +2,20 @@ from api.twitch_api import TwitchAPI
 from constants.endpoints import *
 from constants.parameter_keys import TO_ID_KEY
 
+"""
+    Represents interaction with the Twitch Users API.
+"""
+
 class TwitchUsersAPI(TwitchAPI):
     def __init__(self, client_id, access_token=None):
         super().__init__(client_id, access_token)
         self.base_endpoint = f"{HELIX_ENDPOINT}{USERS_ENDPOINT}"
         self.follows_endpoint = f"{self.base_endpoint}{FOLLOWS_ENDPOINT}"
 
+    '''
+        Sends a GET request to the Twitch User Followers API for the 
+        given user_id and params and returns the json response.
+    '''
     def getUserFollowers(self, user_id, params=None):
         endpoint = f"{self.follows_endpoint}?{TO_ID_KEY}={user_id}"
         return self._request_get(endpoint, params)
